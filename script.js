7f879bc990d680e2f18c5d0e8e32e3080d0db878
Palash Rawat <palash.rawat@gmail.com>
2021-05-26 21:02:03 +0530

    Index file updates

---
+++ script.js
@@ -0,0 +1,32 @@
+const projects = [
+   'four-card-feature-section-master',
+   'huddle-landing-page-with-single-introductory-section-master',
+   'intro-component-with-signup-form-master',
+   'single-price-grid-component-master'
+];
+
+const list= document.getElementById('list');
+
+projects.forEach((project, i) =>{
+    const listItem= document.createElement('li');
+    const link= document.createElement('a');
+    link.href = `/${project}/index.html`;
+    link.innerText= `${i+1}. ${formatProjectName(project)}`;
+
+    const img= document.createElement('img');
+    img.src= `/${project}/design/desktop-design.jpg`;
+
+    link.prepend(img);
+
+    listItem.appendChild(link);
+    list.appendChild(listItem);
+
+});
+
+function formatProjectName(name) {
+	return name
+		.split('-')
+		.map(word => word[0].toUpperCase() + word.slice(1))
+		.join(' ');
+}
+
